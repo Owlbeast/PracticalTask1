6b499a1b9ee03253e480a6ea19635a20e85075af
Maria <maria_praded@stylesoft.by>
2023-01-25 22:32:55 +0100

    1. ui, db connection, getting data added

--- PracticalTask1/Database/DatabaseHandler.cs
+++ PracticalTask1/Database/DatabaseHandler.cs
@@ -6,14 +6,16 @@ namespace PracticalTask1.Database
 {
     public class DatabaseHandler
     {
-        private SQLiteConnection _db;
+        public SQLiteConnection? _db;
+        public string fullPath;
 
         public DatabaseHandler()
         {
             var basePath = Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData);
-            var fullPath =  Path.Combine(basePath, "packagesdb.sqlite");
+            fullPath =  Path.Combine(basePath, "packagesdb.sqlite");
             _db = new SQLiteConnection(fullPath, SQLiteOpenFlags.ReadWrite | SQLiteOpenFlags.Create);
             _db.CreateTable<Package>();
+            _db.Close();
         }
     }
 }
--- PracticalTask1/ViewModels/MainWindowViewModel.cs
+++ PracticalTask1/ViewModels/MainWindowViewModel.cs
@@ -1,12 +1,33 @@
-﻿using System;
+﻿using PracticalTask1.Database;
+using SQLite;
+using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace PracticalTask1.ViewModels
 {
-    internal class MainWindowViewModel
+    public class MainWindowViewModel : INotifyPropertyChanged
     {
+        public DatabaseHandler databaseHandler = new DatabaseHandler();
+        public List<Package> Packages { get; set; }
+
+        public MainWindowViewModel() 
+        {
+            using(var db = new SQLiteConnection(databaseHandler.fullPath, SQLiteOpenFlags.ReadWrite | SQLiteOpenFlags.Create))
+            {
+                Packages = db.Table<Package>().ToList();
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void OnPropertyChanged([CallerMemberName] string prop = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(prop));
+        }
     }
 }
