a9c6bd59463b444f5e13c5c44d2e21d06b9418ad
Maria <maria_praded@stylesoft.by>
2023-01-29 17:08:41 +0100

    added update

--- PracticalTask1/ViewModels/MainWindowViewModel.cs
+++ PracticalTask1/ViewModels/MainWindowViewModel.cs
@@ -1,30 +1,24 @@
 ﻿using CommunityToolkit.Mvvm.Input;
+using GalaSoft.MvvmLight;
 using PracticalTask1.Database;
 using SQLite;
-using System;
-using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.ComponentModel;
-using System.Linq;
 using System.Runtime.CompilerServices;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
 using System.Windows.Input;
 
 namespace PracticalTask1.ViewModels
 {
-    public class MainWindowViewModel : INotifyPropertyChanged
+    public class MainWindowViewModel : ViewModelBase
     {
         public DatabaseHandler databaseHandler = new DatabaseHandler();
-        public List<Package> Packages { get; set; }
+        public ObservableCollection<Package> Packages { get; set; }
+        public SQLiteConnection db;
 
         public MainWindowViewModel() 
         {
-            using(var db = new SQLiteConnection(databaseHandler.fullPath, SQLiteOpenFlags.ReadWrite | SQLiteOpenFlags.Create))
-            {
-                Packages = db.Table<Package>().ToList();
-            }
+            db = new SQLiteConnection(databaseHandler.fullPath, SQLiteOpenFlags.ReadWrite);
+            Packages = new ObservableCollection<Package>(db.Table<Package>());
         }
 
         public event PropertyChangedEventHandler PropertyChanged;
@@ -41,7 +35,11 @@ namespace PracticalTask1.ViewModels
             {
                 _UpdateCommand ??= new RelayCommand(() =>
                     {
-                        MessageBox.Show("update clicked");
+
+                        foreach(var package in Packages)
+                        {
+                            db.InsertOrReplace(package);
+                        }
                     });
                 return _UpdateCommand;
             }
@@ -55,7 +53,8 @@ namespace PracticalTask1.ViewModels
             {
                 _CancelCommand ??= new RelayCommand(() =>
                 {
-                    MessageBox.Show("cancel clicked");
+                    Packages.Clear();
+                    db.Table<Package>().ToList().ForEach(i => Packages.Add(i));
                 });
                 return _CancelCommand;
             }
